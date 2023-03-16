f5fbd4fc81c0078b06c66fff2256e3b475a58586
Zrozen <hs144269@umconnect.umt.edu>
2023-03-16 08:50:01 -0600

    Homework 9

---
+++ Homework 9/skech.js
@@ -0,0 +1,31 @@
+  function setup()
+     {
+     createCanvas(500, 600);
+     }
+function draw()
+     {
+         background(120);
+         fill(253,245,226);
+         strokeWeight(4);
+         stroke('black');
+         ellipse(250, 100, 85, 95,);
+         line(235, 270, 235, 400);
+         line(270, 270, 270, 400);
+         fill(10,24,174);
+         rect(225, 150, 55, 155, 20);
+         line(215, 170, 215, 270);
+         square(200, 150, 35, 10);
+         line(290, 170, 290, 270);
+         square(270, 150, 35, 10);
+         triangle(235, 390, 245, 410, 225,420)
+         triangle(270, 390, 275, 410, 265,420)
+         strokeWeight(10);
+         stroke(58,115,172);
+         point(235, 95);
+         point(265, 95);
+         strokeWeight(4);
+         stroke('black');
+         noFill();
+         arc(250, 120, 25, 25, 0, PI );
+         
+    }
